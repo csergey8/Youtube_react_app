8d90bbeae09b954ef90a2cade1538d10acb6fc98
csergey8 <c.sergey@protonmail.com>
2017-12-02 20:17:47 +0200

    add some components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
-const App = () => {
-    return <div>Hi</div>
+
+
+const API_KEY = 'AIzaSyCeQ7C_TCXoJb87DXvvtZVhpFOKJ_JY6BQ';
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+      );
+  }
 }
 
 
