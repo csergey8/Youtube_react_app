c03f9c69909a7b56a537353e78c8a88063a31de0
csergey8 <c.sergey@protonmail.com>
2017-12-04 10:35:24 +0200

    add search funct.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,7 +4,7 @@ class SearchBar extends Component  {
     constructor(props) {
         super(props);
 
-        this.state = { term: 'Yo'};
+        this.state = { term: ''};
     }
 
     render() {
@@ -12,11 +12,15 @@ class SearchBar extends Component  {
             <div className="search-bar">
             <input
             value={this.state.term} 
-            onChange={event => this.setState({term: event.target.value})}/>
-            <p>{this.state.term}</p>
+            onChange={event => this.onInputChange(event.target.value)}/>
             </div>
         )
     }
+
+    onInputChange(term) {
+        this.setState({ term });
+        this.props.onSearchTermChange(term);
+    };
 
    
     
